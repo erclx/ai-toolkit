7249092766915feec3e2c03c96d4c4edf739093b
Eric <eriiicle@outlook.com>
2026-02-23 04:36:03 +0100

    refactor(commitlint): refactor export style and rules order

--- tooling/base/configs/commitlint.config.js
+++ tooling/base/configs/commitlint.config.js
@@ -1,9 +1,11 @@
-export default {
+const config = {
   extends: ['@commitlint/config-conventional'],
   rules: {
+    'subject-case': [0],
     'header-max-length': [2, 'always', 72],
     'scope-case': [2, 'always', 'lower-case'],
     'subject-full-stop': [2, 'never', '.'],
-    'subject-case': [0],
   },
 }
+
+export default config
